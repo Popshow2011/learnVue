b87465199db7086bf9ca31a14650c5517aa8d0b2
Алексей Кузнецов <popshow2011@gmail.com>
2024-09-02 17:15:45 +0400

    Event Listeners урок пройден

--- src/components/DeclarativeRendering/DeclarativeRendering.vue
+++ src/components/DeclarativeRendering/DeclarativeRendering.vue
@@ -5,6 +5,7 @@ import { type CounterType, type MessageType } from './types'
 
 const counter = reactive<CounterType>({ count: 0 })
 const message = ref<MessageType>({ value: 'Hello World!' })
+const color = ref('#291919')
 
 const updateCounter = () => {
   counter.count++
@@ -16,13 +17,21 @@ const updateCounter = () => {
 </script>
 
 <template>
-  <h1>{{ message.value }}</h1>
-  <p>Count is: {{ counter.count }}</p>
-  <button @click="updateCounter" class="btn">Increment</button>
-  <AttributeBindings :alertClass="counter.count < 10 ? 'success' : 'error'" />
+  <div :style="{ backgroundColor: color }" class="container">
+    <h1>{{ message.value }}</h1>
+    <p>Count is: {{ counter.count }}</p>
+    <button @click="updateCounter" class="btn">Increment</button>
+    <AttributeBindings :alertClass="counter.count < 10 ? 'success' : 'error'" />
+    <input type="text" :value="color" />
+  </div>
 </template>
 
 <style scoped>
+.container {
+  padding: 50px;
+  border-radius: 4px;
+  box-shadow: 0 0 1px 1px #aaaaaa;
+}
 .btn {
   user-select: none;
   background-color: #4caf50;

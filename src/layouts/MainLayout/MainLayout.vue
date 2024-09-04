710b29d84f5fbc7176cb92a22a7d4332059a4c31
Алексей Кузнецов <popshow2011@gmail.com>
2024-09-04 14:29:20 +0400

    feat(TodosTable) добавлен компонент таблицы и запрос к апи

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -1,6 +1,10 @@
 <template>
-  <DeclarativeRendering />
+  <div>
+    <DeclarativeRendering />
+    <TodosTable />
+  </div>
 </template>
 <script setup lang="ts">
 import DeclarativeRendering from '@/components/DeclarativeRendering/DeclarativeRendering.vue'
+import TodosTable from '@/components/TodosTable/TodosTable.vue'
 </script>

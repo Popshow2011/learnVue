edf390d9a5308c2a295135ef59a5d87c0d456441
Алексей Кузнецов <popshow2011@gmail.com>
2024-09-06 09:18:03 +0400

    feat(TodosTable) добавлен файл таблицы и запрос к апи

---
+++ src/components/TodosTable/TodosTable.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { type TodoListType } from './types'
+const todos = ref<TodoListType | []>([])
+onMounted(() => {
+  getTodosList()
+})
+const getTodosList = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((json) => (todos.value = json))
+}
+</script>
+<template>
+  <div class="table">
+    <table border="2">
+      <tbody>
+        <tr>
+          <th>id</th>
+          <th>Title</th>
+          <th>completed</th>
+          <th>UserId</th>
+        </tr>
+        <tr v-for="todo in todos" :key="todo.id">
+          <td>{{ todo.id }}</td>
+          <td>{{ todo.title }}</td>
+          <td>{{ todo.completed }}</td>
+          <td>{{ todo.userId }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>

361a08cee2df11b2048515ba14f6bc8c4e9ef9df
Алексей Кузнецов <popshow2011@gmail.com>
2024-09-17 14:50:05 +0400

    feat(HeaderComponent) изучены Components добавлен хидер для приложения

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+    <Header />
     <DeclarativeRendering />
     <TodosTable />
   </div>
 </template>
 <script setup lang="ts">
 import DeclarativeRendering from '@/components/DeclarativeRendering/DeclarativeRendering.vue'
+import Header from '@/components/Header/HeaderComponent.vue'
 import TodosTable from '@/components/TodosTable/TodosTable.vue'
 </script>
